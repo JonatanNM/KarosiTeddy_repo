d7dc9bf05ef1d40246ae337bd2c2cad6e9025111
Mireia <mireiagilabertdesign@gmail.com>
2017-02-02 16:14:41 +0100

    añadido alta clientes

--- 01_PROYECTO_KAROSITEDDY/src/datos/ColeccionClientesImpl.java
+++ 01_PROYECTO_KAROSITEDDY/src/datos/ColeccionClientesImpl.java
@@ -23,15 +23,41 @@ public class ColeccionClientesImpl implements IColeccionClientes{
         this.listadoCli = listadoCli;
     }
 
-	@Override
-	public void altaClientes() {
-		// TODO Auto-generated method stub
-		
+    @Override
+	public void altaCliente() {
+		Cliente c = new Cliente();
+		c.crearCliente();
+		altaCliente(c);
 	}
-
+    
 	@Override
-	public void altaClientes(Cliente c) {
-		// TODO Auto-generated method stub
+	public void altaCliente(Cliente c) {
+		DatosBBDD bd = new DatosBBDD();
+		Connection cn = null;
+		try {
+			cn = bd.obtenerConexion();
+			String sql1 = "INSERT INTO persona(nombre_per, apellidos, email, password)";
+			sql1 += "VALUES(?,?,?,?)"; // Es una extensión del código de arriba
+			PreparedStatement st = cn.prepareStatement(sql);
+			st.setString(1, c.getNombre_per());
+			st.setString(2, c.getApellidos_per());
+			st.setString(3, c.getEmail());
+			st.setString(4, c.getPassword());
+			st.execute();
+			
+			String sql2 = " INSERT INTO cliente(fecha_nacimiento, telefono, id_persona)";
+			st = cn.prepareStatement(sql2);
+			st.setString(5, c.getFecha_nacimiento());
+			st.setInt(6, c.getTelefono());
+			st.setInt(7, c.getId_persona());
+			st.execute();
+
+			System.out.println("¡Enhorabuena! ***** ¡Has dado de alta al Cliente " + c.getNombre_per() + "!");
+		} catch (SQLException ex) {
+			ex.getMessage();
+		} finally {
+			bd.cerrarConexion(cn);
+		}
 		
 	}
 
